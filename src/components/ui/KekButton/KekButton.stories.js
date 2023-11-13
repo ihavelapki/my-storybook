7a3ea14a8385899c8c078a62733e9ff0bbfdc92b
ihavelapki <kotlapkovskij@gmail.com>
2023-11-13 22:07:43 +0300

    Config option has been added

--- src/components/ui/KekButton/KekButton.stories.js
+++ src/components/ui/KekButton/KekButton.stories.js
@@ -3,6 +3,32 @@ import KekButton from ".";
 export default {
     title: 'UI/Buttons/KekButton',
     components: KekButton,
+    argTypes: {
+        variant: {
+            type: 'string',
+            description: 'View type',
+            defaultValue: 'main',
+            options: ['main', 'kek', 'lol', 'link', 'square'],
+            control: {
+                type: 'radio'
+            }
+        },
+        size: {
+            type: 'string',
+            description: 'Size',
+            defaultValue: 'medium',
+            options: ['small', 'medium', 'large'],
+            control: {
+                type: 'radio'
+            }
+        },
+        children: {
+            type: 'string',
+            description: 'Name',
+            defaultValue: 'KEK ME',
+            name: 'Kek:'
+        }
+    }
 
 }
 
